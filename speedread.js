96b4960a4c6015d40160f848b280f145cabfa37f
MarkWithall <scroog1@gmail.com>
2013-01-09 13:45:15 +0000

    Through JSLint

--- speedread.js
+++ speedread.js
@@ -1,62 +1,59 @@
 function speedRead() {
-    var html = getSelectionHtml();
-    var text = strip(html);
-    var words = splitIntoWords(text);
-    displayWords(words);
+    "use strict";
+    /*jslint browser: true*/
 
     function getSelectionHtml() {
-        var html = "";
-        if (typeof window.getSelection != "undefined") {
-            var sel = window.getSelection();
+        var html = "", sel, container, i, len;
+        if (window.getSelection !== "undefined") {
+            sel = window.getSelection();
             if (sel.rangeCount) {
-                var container = document.createElement("div");
-                for (var i = 0, len = sel.rangeCount; i < len; ++i) {
+                container = document.createElement("div");
+                for (i = 0, len = sel.rangeCount; i < len; i += 1) {
                     container.appendChild(sel.getRangeAt(i).cloneContents());
                 }
                 html = container.innerHTML;
             }
-        } else if (typeof document.selection != "undefined") {
-            if (document.selection.type == "Text") {
+        } else if (document.selection !== "undefined") {
+            if (document.selection.type === "Text") {
                 html = document.selection.createRange().htmlText;
             }
         }
         return html;
     }
 
-    function strip(html)
-    {
+    function strip(html) {
         var tmp = document.createElement("DIV");
         tmp.innerHTML = html;
-        return tmp.textContent||tmp.innerText;
+        return tmp.textContent || tmp.innerText;
     }
 
-    function splitIntoWords(text)
-    {
+    function splitIntoWords(text) {
         return text.split(" ");
     }
 
-    function displayWords(words)
-    {
-        var i = 0;
-        var oldBody = document.body.innerHTML;
-        var interval = setInterval(function() {
-            if (i >= words.length) {
-                clearInterval(interval);
-                document.body.innerHTML = oldBody;
-            } else {
-                displayWord(words[i]);
-                i++;
-            }
-        }, 150);
-    }
-
-    function displayWord(word)
-    {
-        var newBody = document.createElement('body');
-        var p = document.createElement('p');
+    function displayWord(word) {
+        var newBody = document.createElement('body'), p;
+        p = document.createElement('p');
         p.style.cssText = "text-align: left !important; background-color: white !important; color: black !important; font-size: 40px !important; margin: 20px !important;";
         p.appendChild(document.createTextNode(word));
         newBody.appendChild(p);
         document.body.innerHTML = newBody.innerHTML;
     }
+
+    function displayWords(words) {
+        var i = 0, oldBody, interval;
+        oldBody = document.body.innerHTML;
+        interval = setInterval(function () {
+            if (i >= words.length) {
+                clearInterval(interval);
+                document.body.innerHTML = oldBody;
+            } else {
+                displayWord(words[i]);
+                i += 1;
+            }
+        }, 150);
+    }
+
+    displayWords(splitIntoWords(strip(getSelectionHtml)));
 }
+
