65db0266911e66b01ecdf687a4edae6e842f8d2f
Mark Withall <github@freudiantypo.com>
2013-01-11 21:22:43 +0000

    Tidied build script.

--- build.sh
+++ build.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-cat speedread.js >/tmp/speedread.js
+JS_FILE=speedread.js
+OUTPUT_HTML=index.html
+
+# Requires closure-compiler (https://developers.google.com/closure/compiler/)
+COMPILER_JAR=compiler.jar
+
+# Add call to main function to js
+cat $JS_FILE >/tmp/speedread.js
 echo "speedRead();" >>/tmp/speedread.js
-java -jar compiler.jar --compilation_level=ADVANCED_OPTIMIZATIONS --formatting=SINGLE_QUOTES --js=/tmp/speedread.js | sed s/%/%25/ >/tmp/speedread2.js
-cat index_header.html /tmp/speedread2.js index_footer.html >index.html
+
+# Optimise and minify
+java -jar $COMPILER_JAR --compilation_level=ADVANCED_OPTIMIZATIONS --formatting=SINGLE_QUOTES --js=/tmp/speedread.js | sed s/%/%25/ >/tmp/speedread2.js
+
+# Build html file
+cat index_header.html /tmp/speedread2.js index_footer.html >$OUTPUT_HTML
+
