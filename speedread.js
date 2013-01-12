3d8e6091555590efae54e548c2597aaaeebde872
Mark Withall <github@freudiantypo.com>
2013-01-12 20:56:25 +0000

    Changed to not nested for testing.

--- speedread.js
+++ speedread.js
@@ -1,83 +1,83 @@
+'use strict';
+/*jslint browser: true*/
+
+function getSelectionText() {
+    if (window.getSelection) {
+        return window.getSelection();
+    }
+    if (document.getSelection) {
+        return document.getSelection();
+    }
+    if (document.selection) {
+        return document.selection.createRange().text;
+    }
+    return '';
+}
+
+function strip(html) {
+    var tmp = document.createElement('DIV');
+    tmp.innerHTML = html;
+    return tmp.textContent || tmp.innerText;
+}
+
+function splitIntoWords(text) {
+    return text.split(/\s+/);
+}
+
+function displayWord(word) {
+    var srDialog = document.getElementById('srDialog'), p, oldWord;
+    oldWord = document.getElementById('srWord');
+    if (oldWord !== null) {
+        srDialog.removeChild(oldWord);
+    }
+    p = document.createElement('p');
+    p.id = 'srWord';
+    /* NOTE: percentages need to be e.g. 100%25 in bookmarklets! */
+    p.style.cssText = 'text-align: center; background-color: white; color: black; font-size: 40px; position: fixed; top: 50%; left: 50%; width: 400px; margin-left: -200px; height: 100px; margin-top: -50px;';
+    p.appendChild(document.createTextNode(word));
+    srDialog.appendChild(p);
+}
+
+function closeDialogOnEsc(interval) {
+    document.body.onkeydown = function(evt) {
+        if (window.event.keyCode === 27) {
+            evt.preventDefault(); /* stop Mac Safari exiting full screen */
+            clearInterval(interval);
+            removeDialog();
+        }
+    };
+}
+
+function createDialog() {
+    var srDialog = document.createElement('div');
+    srDialog.id = 'srDialog';
+    srDialog.style.cssText = 'background-color: white; opacity: .95; filter: alpha(opacity=95); position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 1000;';
+    document.body.appendChild(srDialog);
+    srDialog.focus();
+}
+
+function removeDialog() {
+    var srDialog = document.getElementById('srDialog');
+    if (srDialog !== null) {
+        document.body.removeChild(srDialog);
+    }
+}
+
+function displayWords(words) {
+    var i = 0;
+    createDialog();
+    var interval = setInterval(function () {
+        if (i >= words.length) {
+            clearInterval(interval);
+            removeDialog();
+        } else {
+            displayWord(words[i]);
+            i += 1;
+        }
+    }, 225);
+    closeDialogOnEsc(interval);
+}
+
 function speedRead() {
-    'use strict';
-    /*jslint browser: true*/
-
-    function getSelectionText() {
-        if (window.getSelection) {
-            return window.getSelection();
-        }
-        if (document.getSelection) {
-            return document.getSelection();
-        }
-        if (document.selection) {
-            return document.selection.createRange().text;
-        }
-        return '';
-    }
-
-    function strip(html) {
-        var tmp = document.createElement('DIV');
-        tmp.innerHTML = html;
-        return tmp.textContent || tmp.innerText;
-    }
-
-    function splitIntoWords(text) {
-        return text.split(/\s+/);
-    }
-
-    function displayWord(word) {
-        var srDialog = document.getElementById('srDialog'), p, oldWord;
-        oldWord = document.getElementById('srWord');
-        if (oldWord !== null) {
-            srDialog.removeChild(oldWord);
-        }
-        p = document.createElement('p');
-        p.id = 'srWord';
-        /* NOTE: percentages need to be e.g. 100%25 in bookmarklets! */
-        p.style.cssText = 'text-align: center; background-color: white; color: black; font-size: 40px; position: fixed; top: 50%; left: 50%; width: 400px; margin-left: -200px; height: 100px; margin-top: -50px;';
-        p.appendChild(document.createTextNode(word));
-        srDialog.appendChild(p);
-    }
-
-    function closeDialogOnEsc(interval) {
-        document.body.onkeydown = function(evt) {
-            if (window.event.keyCode === 27) {
-                evt.preventDefault(); /* stop Mac Safari exiting full screen */
-                clearInterval(interval);
-                removeDialog();
-            }
-        };
-    }
-
-    function createDialog() {
-        var srDialog = document.createElement('div');
-        srDialog.id = 'srDialog';
-        srDialog.style.cssText = 'background-color: white; opacity: .95; filter: alpha(opacity=95); position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 1000;';
-        document.body.appendChild(srDialog);
-        srDialog.focus();
-    }
-
-    function removeDialog() {
-        var srDialog = document.getElementById('srDialog');
-        if (srDialog !== null) {
-            document.body.removeChild(srDialog);
-        }
-    }
-
-    function displayWords(words) {
-        var i = 0;
-        createDialog();
-        var interval = setInterval(function () {
-            if (i >= words.length) {
-                clearInterval(interval);
-                removeDialog();
-            } else {
-                displayWord(words[i]);
-                i += 1;
-            }
-        }, 225);
-        closeDialogOnEsc(interval);
-    }
-
     displayWords(splitIntoWords(strip(getSelectionText())));
 }
