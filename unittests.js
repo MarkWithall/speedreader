72172167eb4ad8ef7a292fc733c928f0af656f8d
Mark Withall <github@freudiantypo.com>
2013-01-12 20:56:37 +0000

    Started adding tests using qunit

---
+++ unittests.js
@@ -0,0 +1,32 @@
+function selectText(element) {
+    var doc = document
+        , text = doc.getElementById(element)
+        , range, selection
+    ;    
+    if (doc.body.createTextRange) { //ms
+        range = doc.body.createTextRange();
+        range.moveToElementText(text);
+        range.select();
+    } else if (window.getSelection) { //all others
+        selection = window.getSelection();        
+        range = doc.createRange();
+        range.selectNodeContents(text);
+        selection.removeAllRanges();
+        selection.addRange(range);
+    }
+}
+
+module('getSelectionText');
+
+test('simpleSelection', function() {
+    selectText('testContent');
+    var result = getSelectionText();
+    return equal(result, 'Test content\n\nThis is a test!');
+});
+
+module('splitIntoWords');
+
+test('simpleTest', function() {
+    var result = splitIntoWords("a b c d");
+    return deepEqual(result, ['a', 'b', 'c', 'd']);
+});
