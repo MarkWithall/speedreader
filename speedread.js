66a16cd3daf7e2df41390caccc046b341c3111df
Mark Withall <github@freudiantypo.com>
2013-01-18 19:26:12 +0000

    More private vars and renamed.

--- speedread.js
+++ speedread.js
@@ -41,46 +41,46 @@ function ElementCreator() {
 }
 
 function SrDialog() {
-    var dialog = null;
-    var p = null;
+    var _dialog = null;
+    var _p = null;
 
     this.create = function() {
         var creator = new ElementCreator();
 
-        dialog = creator.createDiv('srDialog', 'background-color: white; opacity: .95; filter: alpha(opacity=95); position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 1000;');
+        _dialog = creator.createDiv('srDialog', 'background-color: white; opacity: .95; filter: alpha(opacity=95); position: fixed; top: 0; left: 0; width: 100%; height: 100%; z-index: 1000;');
 
         /* NOTE: percentages need to be changed to e.g. 100%25 for inline bookmarklets! */
-        p = creator.createPara('srWord', 'text-align: center; background-color: white; color: black; font-size: 40px; position: fixed; top: 50%; left: 50%; width: 400px; margin-left: -200px; height: 100px; margin-top: -50px;');
+        _p = creator.createPara('srWord', 'text-align: center; background-color: white; color: black; font-size: 40px; position: fixed; top: 50%; left: 50%; width: 400px; margin-left: -200px; height: 100px; margin-top: -50px;');
 
-        p.innerText = '';
-        dialog.appendChild(p);
-        document.body.appendChild(dialog);
-        dialog.focus();
+        _p.innerText = '';
+        _dialog.appendChild(_p);
+        document.body.appendChild(_dialog);
+        _dialog.focus();
     };
 
     this.remove = function() {
-        if (dialog !== null) {
-            document.body.removeChild(dialog);
+        if (_dialog !== null) {
+            document.body.removeChild(_dialog);
         }
     };
 
     this.showWord = function(word) {
-        p.innerText = word;
+        _p.innerText = word;
     };
 }
 
 function WordDisplayer(words, srDialog, finished) {
-    this.words = words;
-    this.srDialog = srDialog;
-    this.finished = finished;
-    this.i = 0;
+    var _words = words;
+    var _srDialog = srDialog;
+    var _finished = finished;
+    var _i = 0;
 
     this.nextWord = function() {
-        if (this.i >= this.words.length) {
-            this.finished();
+        if (_i >= _words.length) {
+            finished();
         } else {
-            this.srDialog.showWord(this.words[this.i]);
-            this.i += 1;
+            _srDialog.showWord(_words[_i]);
+            _i += 1;
         }
     };
 }
