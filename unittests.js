558becf0d0657ec8a4c9a652125ea60cbb8e3961
Mark Withall <github@freudiantypo.com>
2013-01-23 18:33:06 +0000

    Made test create and remove data.

--- unittests.js
+++ unittests.js
@@ -1,3 +1,22 @@
+function createText() {
+    var testContent = document.createElement('div');
+    testContent.id = 'testContent';
+    var header = document.createElement('h1');
+    var para = document.createElement('p');
+    header.innerHTML = 'Test content';
+    para.innerHTML = 'This is a test!';
+    testContent.appendChild(header);
+    testContent.appendChild(para);
+    document.body.appendChild(testContent);
+}
+
+function removeText() {
+    var testContent = document.getElementById('testContent');
+    if (testContent !== null) {
+        document.body.removeChild(testContent);
+    }
+}
+
 function selectText(element) {
     var doc = document
         , text = doc.getElementById(element)
@@ -19,10 +38,12 @@ function selectText(element) {
 module('getSelectionText');
 
 test('simpleSelection', function() {
+    createText();
     selectText('testContent');
     var result = new Page(window, document).getSelectedText();
     notEqual(result.indexOf("Test content"), -1);
     notEqual(result.indexOf("This is a test!"), -1);
+    removeText();
 });
 
 module('splitIntoWords');
