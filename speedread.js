f5ea5eae4ef2fd6bed693853c309294d6c168ec1
MarkWithall <scroog1@gmail.com>
2013-02-04 13:52:57 +0000

    Another attempt

--- speedread.js
+++ speedread.js
@@ -54,7 +54,7 @@ function appendEmptyElement(array) {
 
 var splitIntoSentences = function(text) {
     return stringReverse(text)
-             .split(/(?=[.:?!]+)/)
+             .split(/(?=[\s\n\r]+["']?[.?!]+)/)
              .map(stringReverse)
              .reverse()
              .map(splitIntoWords)
